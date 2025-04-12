b4ed4a4771cce004c9206664e6cbfb45a15acfc2
litetex <40789489+litetex@users.noreply.github.com>
2025-04-12 21:52:47 +0200

    Update MinecraftServerStartStopMixin.java

--- src/main/java/net/litetex/oie/mixin/MinecraftServerStartStopMixin.java
+++ src/main/java/net/litetex/oie/mixin/MinecraftServerStartStopMixin.java
@@ -14,6 +14,7 @@ import net.minecraft.server.MinecraftServer;
 @Mixin(MinecraftServer.class)
 public abstract class MinecraftServerStartStopMixin
 {
+	@SuppressWarnings("javabugs:S6320")
 	@Inject(at = @At(value = "INVOKE",
 		target = "Lnet/minecraft/server/MinecraftServer;createMetadata()Lnet/minecraft/server/ServerMetadata;",
 		ordinal = 0),
